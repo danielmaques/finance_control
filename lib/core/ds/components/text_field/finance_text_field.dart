a228d58f61a4d14b4052a9ced43abae96bd54c80
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-09-02 14:33:50 -0300

    Text

---
+++ lib/core/ds/components/text_field/finance_text_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FinanceTextField extends StatelessWidget {
+  const FinanceTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField();
+  }
+}
