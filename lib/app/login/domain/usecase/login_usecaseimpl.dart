6ee3f79eebb1e55013e77d4040f2c7cb65da45db
guilherme <guylherme_mas@hotmail.com>
2023-09-05 13:40:35 -0300

    newfolder login page

---
+++ lib/app/login/domain/usecase/login_usecaseimpl.dart
@@ -0,0 +1,14 @@
+import 'package:finance_control/app/login/datasource/repository/login_repository.dart';
+import 'package:finance_control/app/login/domain/usecase/login_usecase.dart';
+import 'package:finance_control/core/model/user_model.dart';
+
+class LoginUseCaseImpl implements LoginUseCase {
+  final LoginRepository loginRepository;
+
+  LoginUseCaseImpl(this.loginRepository);
+
+  @override
+  Future<UserModel> login(String email, String password) async {
+    return await loginRepository.login(email, password);
+  }
+}
