4b4938407ecb6b299ad74da263eefb66ef027d0f
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-09-13 07:03:08 -0300

    camera

--- lib/app/add_transaction/ui/controller/add_transaction_controller.dart
+++ lib/app/add_transaction/ui/controller/add_transaction_controller.dart
@@ -1,43 +1,59 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class AddTransactionController {
-  final ImagePicker _picker = ImagePicker();
+  CameraController? cameraController;
   final ValueNotifier<XFile?> selectedImageNotifier =
       ValueNotifier<XFile?>(null);
   final ValueNotifier<List<XFile?>> imagesNotifier =
       ValueNotifier<List<XFile?>>([]);
 
+  AddTransactionController() {
+    initializeCamera();
+  }
+
+  Future<void> initializeCamera() async {
+    try {
+      final cameras = await availableCameras();
+      if (cameras.isNotEmpty) {
+        final firstCamera = cameras.first;
+
+        cameraController = CameraController(
+          firstCamera,
+          ResolutionPreset.medium,
+        );
+
+        await cameraController!.initialize();
+      } else {
+        if (kDebugMode) {
+          print('No cameras available');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error initializing camera: $e');
+      }
+    }
+  }
+
   Future<void> pickImageFromGallery() async {
     bool hasPermission = await requestGalleryPermission();
     if (!hasPermission) return;
 
-    _pickImage(ImageSource.gallery);
-  }
-
-  Future<void> pickImageFromCamera() async {
-    bool hasPermission = await requestCameraPermission();
-    if (!hasPermission) return;
-
-    _pickImage(ImageSource.camera);
-  }
-
-  Future<void> _pickImage(ImageSource source) async {
-    try {
-      final XFile? image = await _picker.pickImage(source: source);
-      if (image != null) {
-        imagesNotifier.value = [...imagesNotifier.value, image];
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print('Erro ao selecionar imagem: $e');
-      }
+    final XFile? image =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      imagesNotifier.value = [...imagesNotifier.value, image];
     }
   }
 
   void dispose() {
     selectedImageNotifier.dispose();
+    if (cameraController != null && cameraController!.value.isInitialized) {
+      cameraController!.dispose();
+    }
   }
 
   Future<bool> requestGalleryPermission() async {
